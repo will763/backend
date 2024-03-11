3c9664ee59b7f16c06c2125cc10fc45bb630a7a3
William Lisboa <66382974+will763@users.noreply.github.com>
2024-03-11 11:04:09 -0300

    Update setRedirectUrl.ts

--- src/auth/middleware/setRedirectUrl.ts
+++ src/auth/middleware/setRedirectUrl.ts
@@ -13,7 +13,6 @@ export async function setRedirectUrl(req: FastifyRequest, reply: FastifyReply) {
         path:'/',
         httpOnly:true,
         secure:true,
-        sameSite:'none',
-        partitioned: true
+        sameSite:'lax',
     })
 };
